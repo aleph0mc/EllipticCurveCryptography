348436cf47f8e4f9ac4472aa795e6b6d0d0ac4d1
aleph0mc <aleph0mc@gmail.com>
2018-03-23 21:02:36 +0100

    Added elliptic curve M-383

--- ConsoleEllipticCurvesSample/Program.cs
+++ ConsoleEllipticCurvesSample/Program.cs
@@ -10,24 +10,43 @@ namespace ConsoleEllipticCurvesSample
     {
         static void Main(string[] args)
         {
+            // Elliptic curve chosen
+            var ecType = EllipticCurveType.M383;
+
+            Console.WriteLine("Elliptic curve type: {0}", ecType.ToString());
+            Console.WriteLine();
+
             // only Alice knows
             var aliceSecretKey = BigInteger.Parse("55179115824979878594564946684576670362812219109178118526265814188406326272077");
             // only Bob knows
             var bobSecretKey = BigInteger.Parse("28186466892849679686038856807396267537577176687436853369");
             // public keys
             // Alice's public key
-            var pa = EcCryptographyHelper.SecP256k1KeyPairGenerator(aliceSecretKey);
+            EcModPoint pa = null;
             // Bob's public key
-            var pb = EcCryptographyHelper.SecP256k1KeyPairGenerator(bobSecretKey);
+            EcModPoint pb = null;
+            switch (ecType)
+            {
+                case EllipticCurveType.SEC256K1:
+                    pa = EcCryptographyHelper.SecP256k1KeyPairGenerator(aliceSecretKey);
+                    pb = EcCryptographyHelper.SecP256k1KeyPairGenerator(bobSecretKey);
+                    break;
+                case EllipticCurveType.M383:
+                    pa = EcCryptographyHelper.M383KeyPairGenerator(aliceSecretKey);
+                    pb = EcCryptographyHelper.M383KeyPairGenerator(bobSecretKey);
+                    break;
+                default:
+                    break;
+            }
 
             // Alice sends a message to Bob
             string message = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, assumenda beatae ipsum similique dignissimos odit officia dolores laudantium quis dicta.";
             Console.WriteLine("Alice writes to Bob: {0}", message);
 
             // Alice encrypts the message using Bob's public key
-            var encryptStr = EcCryptographyHelper.EncryptSecP256k1Json(message, pb);
+            var encryptStr = EcCryptographyHelper.EcEncryptJson(message, pb, ecType);
             // Bob decrypts the message using his private key
-            var decryptedMsg = EcCryptographyHelper.DecryptSecP256k1Json(encryptStr, bobSecretKey);
+            var decryptedMsg = EcCryptographyHelper.EcDecryptJson(encryptStr, bobSecretKey, ecType);
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Bob reads: {0}", decryptedMsg);
@@ -39,9 +58,9 @@ namespace ConsoleEllipticCurvesSample
             Console.WriteLine("Bob writes to Alice: {0}", message);
 
             // Bob encrypts the message using Alice's public key
-            encryptStr = EcCryptographyHelper.EncryptSecP256k1Json(message, pa);
+            encryptStr = EcCryptographyHelper.EcEncryptJson(message, pa, ecType);
             // Alice decrypts the message using her private key
-            decryptedMsg = EcCryptographyHelper.DecryptSecP256k1Json(encryptStr, aliceSecretKey);
+            decryptedMsg = EcCryptographyHelper.EcDecryptJson(encryptStr, aliceSecretKey, ecType);
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Alice reads: {0}", decryptedMsg);
